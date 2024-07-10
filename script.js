f3800de43b857652250ec94c3e683b529be968f6
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-07-11 00:15:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById("btn").onclick = (e)=>{
+    e.preventDefault();
+    let age = document.getElementById("age").value;
+    let name = document.getElementById("name").value;
+    
+    if(age>18){
+        setTimeout(()=>{
+            alert("Welcome "+name+". You can vote.");
+        },4000)
+    }
+    else{
+        setTimeout(()=>{
+            alert("Oh sorry "+name+". You aren't old enough.");
+        },4000)
+    }
+}
